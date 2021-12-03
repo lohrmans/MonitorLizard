678e6e51de6e972fa5bd91353845f7f4a767db7d
lohrmans <lohrmans@gmatc.matc.edu>
2021-12-03 12:35:29 -0600

    Updated MainActivity.java

--- app/src/main/java/com/example/monitorlizard/MainActivity.java
+++ app/src/main/java/com/example/monitorlizard/MainActivity.java
@@ -2,14 +2,28 @@ package com.example.monitorlizard;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button btnNewMeal, btnViewMeals;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnNewMeal = findViewById(R.id.btnNewMeal);
+        btnViewMeals = findViewById(R.id.btnViewMeals);
+
+        btnNewMeal.setOnClickListener(v -> startActivity(new Intent(MainActivity.this, Meal.class)));
+
+        btnViewMeals.setOnClickListener(v -> startActivity(new Intent(MainActivity.this, MealList.class)));
+
+
     }
 
 
--- app/src/main/java/com/example/monitorlizard/MealList.java
+++ app/src/main/java/com/example/monitorlizard/MealList.java
@@ -4,7 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MealListActivity extends AppCompatActivity {
+public class MealList extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
