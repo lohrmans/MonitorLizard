32db1e2e374b9a3bd5b05fe3083f1871a75146ab
lohrmans <lohrmans@gmatc.matc.edu>
2021-12-03 09:58:16 -0600

    Initial commit

---
+++ app/src/main/java/com/example/monitorlizard/MealListActivity.java
@@ -0,0 +1,14 @@
+package com.example.monitorlizard;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MealListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_meal_list);
+    }
+}
